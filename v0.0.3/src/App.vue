6083ccc3cc8706e06cce5252d0eaec6f40ee04fe
Mikael Helin <mikaelhelin@yahoo.com>
2022-05-09 14:47:12 +0200

    completed v0.0.4

---
+++ v0.0.3/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <h3>Hello World</h3>
+  <h4> {{ counter }}</h4>
+</template>
+
+<script setup>
+  import { useCounterStore } from "./store/useCounter";
+
+  const main = useCounterStore();
+
+  const { counter, name } = main;
+
+// import HelloWorld from './components/HelloWorld.vue'
+
+// export default {
+//   name: 'App',
+//   components: {
+//     HelloWorld
+//   }
+// }
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ v0.0.5/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <h3>Hello World</h3>
+  <h4> {{ counter }}</h4>
+  <button @click="main.addOne">Add one</button>
+</template>
+
+<script setup>
+  import { useCounterStore } from "./store/useCounter";
+  import { storeToRefs } from "pinia";
+
+  const main = useCounterStore();
+
+  const { counter, name } = storeToRefs(main);
+
+// import HelloWorld from './components/HelloWorld.vue'
+
+// export default {
+//   name: 'App',
+//   components: {
+//     HelloWorld
+//   }
+// }
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
