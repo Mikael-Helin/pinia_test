645a38c61ee3d74dc4635c18a06a9308ccc7561c
Mikael Helin <mikaelhelin@yahoo.com>
2022-05-09 14:07:05 +0200

    v0.0.2 completed

--- v0.0.2/src/App.vue
+++ v0.0.2/src/App.vue
@@ -3,6 +3,14 @@
 </template>
 
 <script>
+// import HelloWorld from './components/HelloWorld.vue'
+
+// export default {
+//   name: 'App',
+//   components: {
+//     HelloWorld
+//   }
+// }
 </script>
 
 <style>
